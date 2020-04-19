8d5a25e848f8c3df5b63322101d9236a87398d60
willrockout <wisulliv@ucsc.edu>
2020-04-19 12:03:07 -0700

    Added dropdown menu links.

--- hcat/urls.py
+++ hcat/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('project/<int:pk>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('project', views.ProjectListView.as_view(), name='project_list'),
-    path('project/', views.project_all, name='project_list'),
+    path('project/', views.project_all, name='project_all_list'),
     path('project/covid19/', views.covid_projects, name='covid_project_list')
 ]
 
