58df1bff47165dca8cadfbe3fd4ed032add3db3d
William Sullivan <willrockout@hgwdev.gi.ucsc.edu>
2020-04-20 10:06:03 -0700

    Fixed dropdown namespace issue.

--- hcat/urls.py
+++ hcat/urls.py
@@ -5,7 +5,6 @@ from . import api
 
 app_name = 'hcat'
 urlpatterns = [
-    path('', views.index, name='index'),
     path('project/<int:pk>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('project', views.ProjectListView.as_view(), name='project_list'),
     path('project/', views.project_all, name='project_all_list'),
