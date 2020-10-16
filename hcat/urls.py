d7c0d00cc493999a6719757bd0ab3964b58473dd
willrockout <wisulliv@ucsc.edu>
2020-10-16 16:14:05 -0700

    Cell Browser Dashboard: Created cell browser dashboard html and view. Updated navbar and urls to include paths to cell browser dashboard.

--- hcat/urls.py
+++ hcat/urls.py
@@ -7,6 +7,7 @@ app_name = 'hcat'
 urlpatterns = [
     path('project/', views.project_all, name='project_all_list'),
     path('project/covid19/', views.covid_projects, name='covid_project_list'),
+    path('project/CellBrowser', views.cell_browser_projects, name='cbrowser_project_list'),
     path('project/<str:name_id>/', views.projectdetail, name='project_detail')
 ]
 
