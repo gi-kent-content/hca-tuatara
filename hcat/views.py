aa0b097ce9a3aede42fbfcbb82452a76478e573e
willrockout <wisulliv@ucsc.edu>
2021-04-22 13:53:45 -0700

    Added New Project Submission Functionality: Created new project form and html. Updated urls and views. Added scripts and links to base_site.html

--- hcat/views.py
+++ hcat/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from urllib.parse import unquote
 from .models import Project, SuggestedProjects
 from .suggestion_form import ProjectSuggestion
+from .new_project_form import NewProject, AddTag, AddOrgan, AddOrganPart, AddDisease, AddLab, AddPub, AddUrl, AddContributors, AddLibPrep
 import json
 
 from .models import *
@@ -97,15 +98,84 @@ def project_suggestion(request):
             print('Error in form')
     return render(request, 'hcat/project_suggestion_form.html', {'form': form})
 
-# class ProjectListView(generic.ListView):
-#     template_name = 'hcat/project_list.html'
-#     context_object_name = 'project_list'
-#
-#     def get_queryset(self):
-#         return Project.objects.order_by("-id")[:100]
-#
-# class ProjectDetailView(generic.DetailView):
-#     model=Project
-#     template_name = 'hcat/project_detail.html'
-#     context_object_name = 'project'
+@login_required
+def new_project(request):
+    form = NewProject(prefix="np")
+    tag_form = AddTag(prefix="tag")
+    organ_form = AddOrgan(prefix="organ")
+    opart_form = AddOrganPart(prefix="opart")
+    disease_form = AddDisease(prefix="disease")
+    lib_prep_form = AddLibPrep(prefix="lib_prep")
+    lab_form = AddLab(prefix="lab")
+    pub_form = AddPub(prefix="pub")
+    contrib_form = AddContributors(prefix="contrib")
+    url_form = AddUrl(prefix="url")
+    if request.method == 'POST':
+        if 'fullsubmit' in request.POST:
+            form = NewProject(request.POST, prefix="np")
+            if form.is_valid():
+                form.save()
+                name_id = request.POST.get('np-short_name')
+                return redirect('hcat:project_detail', name_id=name_id)
+            else:
+                print('Error in form')
+        elif 'tagSubmit' in request.POST:
+            tag_form = AddTag(request.POST, prefix="tag")
+            if tag_form.is_valid():
+                tag_form.save()
+            else:
+                print('Error in form')
+        elif 'organSubmit' in request.POST:
+            organ_form = AddOrgan(request.POST, prefix="organ")
+            if organ_form.is_valid():
+                organ_form.save()
+            else:
+                print('Error in form')
+        elif 'opartSubmit' in request.POST:
+            opart_form = AddOrganPart(request.POST, prefix="opart")
+            if opart_form.is_valid():
+                opart_form.save()
+            else:
+                print('Error in form')
+        elif 'diseaseSubmit' in request.POST:
+            disease_form = AddDisease(request.POST, prefix="disease")
+            if disease_form.is_valid():
+                disease_form.save()
+            else:
+                print('Error in form')
+        elif 'LibprepSubmit' in request.POST:
+            lib_prep_form = AddLibPrep(request.POST, prefix="lib_prep")
+            if lib_prep_form.is_valid():
+                lib_prep_form.save()
+            else:
+                print('Error in form')
+        elif 'labSubmit' in request.POST:
+            lab_form = AddLab(request.POST, prefix="lab")
+            if lab_form.is_valid():
+                lab_form.save()
+            else:
+                print('Error in form')
+        elif 'pubSubmit' in request.POST:
+            pub_form = AddPub(request.POST, prefix="pub")
+            if pub_form.is_valid():
+                pub_form.save()
+            else:
+                print('Error in form')
+        elif 'contribSubmit' in request.POST:
+            contrib_form = AddContributors(request.POST, prefix="contrib")
+            if contrib_form.is_valid():
+                contrib_form.save()
+            else:
+                print('Error in form')
+        elif 'urlSubmit' in request.POST:
+            url_form = AddUrl(request.POST, prefix="url")
+            if url_form.is_valid():
+                url_form.save()
+            else:
+                print('Error in form')
+    return render(request, 'hcat_will/new_project_sub.html', {'form': form, 'tag_add': tag_form, 'organ_add': organ_form,
+                                                              'opart_add': opart_form, 'disease_add': disease_form,
+                                                              'lib_prep_add': lib_prep_form, 'lab_add': lab_form,
+                                                              'pub_add': pub_form, 'contrib_add': contrib_form,
+                                                              'url_add': url_form})
 
